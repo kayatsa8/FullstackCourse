d3c5f3155693b27366a2b6dae79252825e692178
kayatsa <kayatsa8@gmail.com>
2025-03-14 10:58:28 +0200

    2.3

--- part2/courseinfo/src/Components/TotalExercises.jsx
+++ part2/courseinfo/src/Components/TotalExercises.jsx
@@ -1,6 +1,8 @@
 const TotalExercises = ({parts}) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+
     return (
-        <h3>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</h3>
+        <h3>total of {total} exercises</h3>
     );
 }
  
